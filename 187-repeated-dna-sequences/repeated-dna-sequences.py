afea9cfb6ba32ee83ea5a1fbc599ed620a7c82ca
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-01 20:33:03 -0700

    Time: 132 ms (5.05%) | Memory: 30.2 MB (19.18%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -4,15 +4,15 @@ class Solution:
         if s_len < 10:
             return []
         counts = collections.Counter()
-        i = 0
+        result = []
         deck = deque(s[:10], maxlen=10)
         counts[''.join(deck)] += 1 
         for char in s[10:]:
             deck.append(char)
             counts[''.join(deck)] += 1
-        result = []
-        for key, val in counts.items():
-            if val >= 2:
-                result.append(key)
+            if counts[''.join(deck)] == 2:
+                result.append(''.join(deck))
         return result
+        
+ 
 
