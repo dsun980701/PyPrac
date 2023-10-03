e76ddb81b6c0e8934782cebfa0e3d7ef35440a37
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-03 09:30:29 -0700

    Time: 46 ms (21.72%) | Memory: 16.1 MB (97.25%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         # First Answer Using sorting, Memory Efficient
-        '''
+
         len_nums = len(nums)
         pair_num = 0
         if len_nums < 2:
@@ -20,6 +20,7 @@ class Solution:
                  r += 1
                  l += 1
         return pair_num
+
         '''
         # Second answer using dict
         a = collections.Counter(nums)
@@ -28,6 +29,6 @@ class Solution:
             if val >= 2:
                 pair_num += math.comb(val, 2)
         return pair_num
-
+        '''
             
 
