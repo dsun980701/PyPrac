dab2baf23f7f14d44329d0c9071436d2be5f284e
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-04 21:50:42 -0700

    Time: 83 ms (94.77%) | Memory: 17.5 MB (22.50%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,5 +1,7 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # Initial answer using collections.Counter, a sub-dict object
+        '''
         len_p = len(p) - 1
         len_s = len(s)
         correct = collections.Counter(p)
@@ -13,4 +15,23 @@ class Solution:
             check[s[i - len_p]] -= 1
             i += 1
         return result
-                
+        '''
+        # optimized answer using hash
+        len_p, len_s = len(p), len(s)
+        if len_p > len_s:
+            return []
+        correct_hash_sum, curr_hash_sum  = 0, 0
+        for i in range(len_p):
+            correct_hash_sum += hash(p[i])
+            curr_hash_sum += hash(s[i])
+        result = []
+        if correct_hash_sum == curr_hash_sum:
+            result.append(0)
+        i = len_p
+        while i < len_s:
+            curr_hash_sum -= hash(s[i - len_p])
+            curr_hash_sum += hash(s[i])
+            if correct_hash_sum == curr_hash_sum:
+                result.append(i - (len_p - 1))
+            i += 1
+        return result
