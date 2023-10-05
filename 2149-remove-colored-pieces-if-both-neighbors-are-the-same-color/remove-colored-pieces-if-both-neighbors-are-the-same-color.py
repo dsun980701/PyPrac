f3d452283eb14788604fac30b4df44988e779583
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-05 10:19:05 -0700

    Time: 1923 ms (5.01%) | Memory: 17.3 MB (45.92%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,5 +1,7 @@
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
+        # Hash method, slow
+        '''
         len_colors = len(colors)
         if len_colors < 3:
             return False 
@@ -15,6 +17,26 @@ class Solution:
         while i < len(colors)-1:
             curr -= hash(colors[i-2])
             curr += hash(colors[i+1])
+            if curr == a:
+                a_count += 1
+            elif curr == b:
+                b_count += 1
+            i += 1
+        return a_count > b_count
+        '''
+        #Counter method
+        a_count, b_count = 0, 0
+        a = collections.Counter('AAA')
+        b = collections.Counter('BBB')
+        curr = collections.Counter(colors[:3])
+        if curr == a:
+            a_count += 1
+        elif curr == b:
+            b_count += 1
+        i = 2
+        while i < len(colors)-1:
+            curr[colors[i-2]] -= 1
+            curr[colors[i+1]] += 1
             if curr == a:
                 a_count += 1
             elif curr == b:
