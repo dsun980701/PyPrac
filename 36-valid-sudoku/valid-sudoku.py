25c065b4f73f6f83eb2803812efb584142d3db37
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-06 17:48:14 -0700

    Time: 105 ms (40.13%) | Memory: 16.3 MB (35.44%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        column = [collections.defaultdict(int) for _ in range(9)]
+        cube =[[collections.defaultdict(int) for _ in range(3)] for _ in range(3)] 
+        r = 0
+        while r < 9:
+            row = collections.defaultdict(int)
+            c = 0
+            while c < 9:
+                num = board[r][c]
+                if num.isnumeric():
+                    row[num] += 1
+                    column[c][num] += 1
+                    cube[r//3][c//3][num] += 1
+                    if row[num] > 1 or column[c][num] > 1 or cube[r//3][c//3][num] > 1:
+                        return False
+                c += 1
+            r += 1
+        return True
+                    
+                    
+                    
