532bbdd8accf8f25d7c77610503e8ac67c642c8a
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-08 15:41:53 -0700

    Time: 405 ms (66.49%) | Memory: 17.5 MB (47.98%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -4,8 +4,10 @@ class Solution:
         for i, num in enumerate(nums):
             if i + num >= maximum_reach:
                 maximum_reach = i + num
+            if maximum_reach >= len(nums) - 1:
+                return True
             if i == maximum_reach:
                 break
-        return maximum_reach >= len(nums) - 1
+        return False
             
             
