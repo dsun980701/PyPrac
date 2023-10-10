3ffb53627e51c8e40c88ad32784b0695d5fccc64
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-10 15:37:07 -0700

    Time: 97 ms (69.14%) | Memory: 19.6 MB (80.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Use hash function
+        
         a = collections.defaultdict(list)
         for string in strs:
+            # Use hash function
+            '''
             key = 0
             for char in string:
                 key += hash(char)
-            a[key].append(string)
+            '''
+            a["".join(sorted(string))].append(string)
         return [a[key] for key in a]
