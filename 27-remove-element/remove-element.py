503db7d3ded3c80a3b5e4e775e75db1eb78ac6f0
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-10 23:38:11 -0700

    Time: 46 ms (20.41%) | Memory: 16.2 MB (53.88%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,5 +1,7 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # Previous Answer, preserving order
+        '''
         length = len(nums)
         while True:
             try:
@@ -7,4 +9,18 @@ class Solution:
                 length -= 1
             except ValueError:
                 break
+        return length
+        '''
+        # Time efficient
+        length = len(nums)
+        i = 0
+        while i < length:
+            if nums[i] == val:
+                while length > 0 and nums[length - 1] == val:
+                    length -= 1
+                if length == i:
+                    break
+                nums[i] = nums[length - 1]
+                length -= 1
+            i += 1
         return length
