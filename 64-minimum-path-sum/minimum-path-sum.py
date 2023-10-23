ae0d5d9827f94bc32855efee6a16897700121323
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-23 12:28:49 -0700

    Time: 90 ms (76.21%) | Memory: 18 MB (65.22%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,5 +1,7 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int: 
+        # Memoized Recursion Method
+        '''
         r_max = len(grid) - 1
         c_max = len(grid[0]) - 1
         memo = {}
@@ -24,3 +26,21 @@ class Solution:
                 memo[(r,c)] = a + grid[r][c]
                 return a + grid[r][c]
         return minPath(0,0)
+        '''
+        # Grid Reduction method
+        r_max = len(grid) 
+        c_max = len(grid[0]) 
+        for r in range(r_max):
+            for c in range(c_max):
+                if r == 0 and c ==0:
+                    continue
+                # Edge case of first row
+                elif r == 0:
+                    grid[r][c] += grid[r][c-1]
+                # Edge case of first column
+                elif c == 0:
+                    grid[r][c] += grid[r-1][c]
+                # Normal cell
+                else:
+                    grid[r][c] += min(grid[r-1][c], grid[r][c-1])
+        return grid[r_max - 1][c_max - 1]
