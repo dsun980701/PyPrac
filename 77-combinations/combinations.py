2c42638056375dd139fc31963f8fd93e22ef1ec8
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-26 18:26:48 -0700

    Time: 889 ms (69.03%) | Memory: 64.8 MB (68.03%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,16 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # Make empty list to store result
         result = []
+        # Recursive function
         def permutate(k, curr):
             # Base Case
             if k == 0:
                 result.append(curr)
                 return
             # Problem Reduction
+            # On the initial stack of the recursion, curr is empty, so there cannot be a reference point to start the combination. Hence, since al of our ranges begin with 1
             if not curr:
                 i = 1
+            # In other cases, reference the previously added num
             else:
                 i = curr[-1] + 1
+            # Reduce the problem
             for j in range(i,n+1):
                 permutate(k-1, curr + [j])
         permutate(k,[])
