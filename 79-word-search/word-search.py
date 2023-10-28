d65f51319461d1480269aabf00509d5f4576bb52
dsun980701 <91859764+dsun980701@users.noreply.github.com>
2023-10-27 17:40:25 -0700

    Time: 2631 ms (89.00%) | Memory: 16.3 MB (47.37%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -2,36 +2,38 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         max_row = len(board)
         max_column = len(board[0])
+        word_len = len(word)
 
-        def have_word(r,c,word):
+        # Function for backtrack
+        def have_word(r,c,word_indx):
             # Base Case
-            if word == "":
+            if word_indx == word_len:
                 return True
             ## Problem Reduction
-            #Up direction
-            c_w = word[0]
+            c_w = word[word_indx]
             if r + 1 < max_row and board[r + 1][c] == c_w:
                 board[r + 1][c] = ""
-                if have_word(r+1,c,word[1:]):
+                if have_word(r+1,c, word_indx + 1):
                     return True
                 board[r + 1][c] = c_w
             if r - 1 >= 0 and board[r - 1][c] == c_w:
                 board[r - 1][c] = ''
-                if have_word(r-1,c,word[1:]):
+                if have_word(r-1,c, word_indx + 1):
                     return True
                 board[r - 1][c] = c_w
             if c + 1 < max_column and board[r][c + 1] == c_w:
                 board[r][c + 1] = ''
-                if have_word(r,c + 1,word[1:]):
+                if have_word(r,c + 1, word_indx + 1):
                     return True
                 board[r][c + 1] = c_w
             if c - 1 >= 0 and board[r][c - 1] == c_w:
                 board[r][c - 1] = ''
-                if have_word(r,c-1,word[1:]):
+                if have_word(r,c-1, word_indx + 1):
                     return True
                 board[r][c - 1] = c_w
             return False
 
+        # Find all the starting locations
         r = 0
         start = []
         while r < max_row:
@@ -41,9 +43,10 @@ class Solution:
                     start.append((r,c))
                 c += 1
             r += 1
+        # Use all the starting locations
         for x in start:
             board[x[0]][x[1]] = ''
-            if have_word(x[0],x[1],word[1:]):
+            if have_word(x[0],x[1],1):
                 return True
             board[x[0]][x[1]] = word[0]
         return False
