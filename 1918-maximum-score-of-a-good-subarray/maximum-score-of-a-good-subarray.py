3441ca9a107547ef202c0d86628de9faa21d2221
David <91859764+dsun980701@users.noreply.github.com>
2023-11-05 09:32:45 -0800

    Time: 1026 ms (77.11%) | Memory: 26.9 MB (57.70%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.py
@@ -1,6 +1,10 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
         # Two pointers from k-th element moving
+        # The key point in this problem is framing the problem
+        # Solver has to understand that we are trying to find the maximum
+        # score where length is rewarded, but how small min value is within the
+        # subarray is penalized.
         result, min_val = nums[k], nums[k]
         len_nums = len(nums)
         r, l = k, k
